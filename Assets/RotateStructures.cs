74c79d49dd6c4c2eef14c0d89a1b5769d7fa76b9
danvettor <danfvettor@gmail.com>
2015-06-28 18:45:55 -0300

    Rotação das moleculas
    Moleculas estão rodando, mas de uma forma muito louca. Só funciona com
    mouse por enquanto.

---
+++ Assets/RotateStructures.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateStructures : MonoBehaviour {
+	public GameObject[] molecularStructure;
+	public float rotationSpeed;
+	private GameObject currentStructure;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (GameObject structure in molecularStructure) {
+			if(structure.activeInHierarchy)
+			{
+				currentStructure = structure;
+			}
+
+		}
+		if (Input.GetMouseButton (0)) {
+			currentStructure.transform.Rotate(Input.mousePosition*Time.deltaTime*rotationSpeed);
+		}
+		else currentStructure.transform.Rotate (Vector3.right*Time.deltaTime*rotationSpeed);
+	
+	}
+}
