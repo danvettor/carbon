ff330ce492e2463546710685e008d059f516f58b
Carlos Sergio de Paiva Araujo <carlossdparaujo@gmail.com>
2015-06-28 23:00:33 -0300

    Alterando cena para só se mover a molécula caso seja clicado na área de visualização.

--- Assets/RotateStructures.cs
+++ Assets/RotateStructures.cs
@@ -1,30 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using System.Collections;
 
-public class RotateStructures : MonoBehaviour {
+public class RotateStructures : MonoBehaviour, IDragHandler {
 	public Camera camera;
 	public GameObject[] molecularStructure;
 	public float rotationSpeed;
 	private GameObject currentStructure;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	public void OnDrag(PointerEventData data) {
 		foreach (GameObject structure in molecularStructure) {
 			if(structure.activeInHierarchy) {
 				currentStructure = structure;
 			}
-
+			
 		}
-
+		
 		if (currentStructure == null) {
-			Debug.Log("deu eruim"); 
 			return;
 		}
-
+		
 		if (Input.GetMouseButton(0)) {
 			currentStructure.transform.Rotate(new Vector3(0.0f, -1*Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")), rotationSpeed);
 		}
