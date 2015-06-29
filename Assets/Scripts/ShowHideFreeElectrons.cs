5071452f36168b276f5cce054096b2b4c799973d
GustavoPMSilva <gustavopmirandasilva@gmail.com>
2015-06-29 00:09:15 -0300

    Botão para mostrar esconder elétrons livres

---
+++ Assets/Scripts/ShowHideFreeElectrons.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+//using System.Collections.Generic;
+
+public class ShowHideFreeElectrons : MonoBehaviour {
+
+	private bool isVisible = true;
+	private ArrayList freeElectronGameObjects;
+
+	void Start () {
+		freeElectronGameObjects = new ArrayList();
+	}
+
+	void Update () {
+		GameObject[] freeElectron = GameObject.FindGameObjectsWithTag("FreeElectron");
+
+		foreach (GameObject go in freeElectron) {
+			if (!freeElectronGameObjects.Contains(go)) {
+				freeElectronGameObjects.Add(go);
+			}
+		}
+	}
+
+	public void showHideElectrons () {
+		isVisible = !isVisible;
+
+		foreach (GameObject go in freeElectronGameObjects) {
+			go.SetActive(isVisible);
+		}
+	}
+}
