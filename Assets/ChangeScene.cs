c35d387cd5f1b63a3ed5d115b29f7a78da66aa5d
danvettor <danfvettor@gmail.com>
2015-06-29 00:29:28 -0300

    Tela inicial e transição
    Troca de tela com o botão do mouse

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mouseInput ();
+	}
+
+	void mouseInput()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			Application.LoadLevel("MainScene");
+			
+		}
+
+	}
+}
