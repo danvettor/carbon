43115039edb87a8e070bc0e155f43940b3259812
danvettor <danfvettor@gmail.com>
2015-06-29 18:20:00 -0300

    Animação nova na intro

---
+++ Assets/HidePopup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HidePopup : MonoBehaviour {
+
+	public GameObject popup;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Hide() {
+		popup.SetActive (false);
+	}
+}
---
+++ Assets/Shutdown.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shutdown : MonoBehaviour {
+
+	public GameObject quitPopup;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void shutdown() {
+		quitPopup.SetActive (true);
+	}
+}
